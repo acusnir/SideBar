af673fd2aeb6a93d09188e89d96a0ec54cf100c9
Anastasia Cusnir <a_cusnir@live.concordia.ca>
2019-02-14 21:49:46 -0500

    Initial commit

---
+++ app/src/main/java/com/concordia/cejv669/sidebar/MainActivity.java
@@ -0,0 +1,78 @@
+package com.concordia.cejv669.sidebar;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
+{
+
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dl = (DrawerLayout)findViewById(R.id.drawer_layout);
+        t = new ActionBarDrawerToggle
+                (this, dl,R.string.nav_open, R.string.nav_close);
+        dl.addDrawerListener(t);
+        t.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView nv = findViewById(R.id.nav_view);
+        nv.setNavigationItemSelectedListener(this);
+
+        dl.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(@NonNull View view, float v) {
+
+            }
+
+            @Override
+            public void onDrawerOpened(@NonNull View view) {
+
+            }
+
+            @Override
+            public void onDrawerClosed(@NonNull View view) {
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int i) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(t.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item)
+    {
+        if (item.getItemId() == R.id.nav_gallery){
+            Toast.makeText(getApplicationContext(), "Your password is gallery", Toast.LENGTH_LONG).show();
+        }
+
+        dl.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+}
